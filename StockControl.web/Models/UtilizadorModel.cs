cff300ce1d781190631fdc3bbcd410627ec3c7df
nuno chev <nunochev@live.com>
2020-08-27 11:24:44 +0200

    Obter Resultado do Commando

--- StockControl.web/Models/UtilizadorModel.cs
+++ StockControl.web/Models/UtilizadorModel.cs
@@ -10,6 +10,7 @@ namespace StockControl.web.Models
     {
        public static bool ValidarUtilizador(string login, string senha)
        {
+            var ret = false;
             using (var conexao = new SqlConnection())
             {
                 conexao.ConnectionString = "";
@@ -22,9 +23,11 @@ namespace StockControl.web.Models
                     //Qery especifica p SQl server
                     comando.CommandText = string.Format(
                         "select count(*) from utilizador where login='{0}' and senha='{1}'", login, senha);
+                    //Validar se retorna alguma informacao
+                   ret= ((int)comando.ExecuteScalar() > 0);
                 }
             }
-          
+            return ret; 
        }
     }
 }
